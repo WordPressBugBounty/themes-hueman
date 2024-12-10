df386ba8e0007ae1f01a05f14501a31a39ae050b
Sean Murphy <sean@iamseanmurphy.com>
2024-12-10 17:17:58 +0000

    3.7.27

--- hueman/functions/czr/panels/class-panels.php
+++ hueman/functions/czr/panels/class-panels.php
@@ -60,8 +60,8 @@ if ( ! class_exists( 'HU_Customize_Panels') ) :
         <h3 class="accordion-section-title" tabindex="0">
         <button type="button" class="accordion-trigger" aria-expanded="false" aria-controls="static_front_page-content">
         {{ data.title }}
-				</button>
-          <span class="czr-panel-subtitle">{{ data.czr_subtitle }}</span>
+        <span class="czr-panel-subtitle">{{ data.czr_subtitle }}</span>
+				</button>          
           <span class="screen-reader-text"><?php _e( 'Press return or enter to open this panel', 'hueman' ); ?></span>
         </h3>
         <ul class="accordion-sub-container control-panel-content"></ul>
